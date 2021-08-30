9d9a1b67259d6e0a645117b85df7d17ef1ec33a1
pierluigi83 <ildragoneblu@libero.it>
2021-08-30 18:30:11 +0200

    setup project

---
+++ android/src/it/pier/mariobros/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package it.pier.mariobros;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import it.pier.mariobros.MarioBros;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MarioBros(), config);
+	}
+}
---
+++ desktop/src/it/pier/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.pier.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.pier.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
